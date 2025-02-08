851ccddb9b15f49869a7d3f4c54b7137f5dd32a6
Sofia Zapata Zuluaga <jzapatso@jci.com>
2025-02-07 20:16:53 -0500

    Commit First

---
+++ movie/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+
+def home(request):
+   # return HttpResponse('<h1>Welcome to Home Page</h1>')
+   #return render(request, 'movie/home.html')
+   return render(request, 'home.html', {'name': 'Sofia Zapata Zuluaga'})
+
+def about(request):
+    # return HttpResponse('<h1>About Movies Review Page</h1><h2>Esta es la página About de mi sitio web</h2>')
+    return render(request, 'movie/about.html')
