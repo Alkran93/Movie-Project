131b47420a5277b50cb19f2d32f9a42ee29550d3
Sofia Zapata Zuluaga <jzapatso@jci.com>
2025-02-09 15:39:51 -0500

    mejoras en la pagina

--- movie/views.py
+++ movie/views.py
@@ -6,15 +6,22 @@ from .models import Movie
 # Create your views here.
 
 def home(request):
-   # return HttpResponse('<h1>Welcome to Home Page</h1>')
+    #return HttpResponse('<h1>Welcome to Home Page</h1>')
    #return render(request, 'movie/home.html')
    #return render(request, 'home.html', {'name': 'Sofia Zapata'})
-   searchTerm = request.GET.get('searchMovie')
-   if searchTerm:
+    name = "Sofia Zapata"  # Definimos la variable antes
+    searchTerm = request.GET.get('searchMovie')
+    
+    if searchTerm:
         movies = Movie.objects.filter(title__icontains=searchTerm)
-   else:
+    else:
         movies = Movie.objects.all()
-   return render(request, 'home.html', {'searchTerm': searchTerm, 'movies': movies})
+    
+    return render(request, 'home.html', {
+        'name': name,  # Pasamos la variable en el contexto
+        'searchTerm': searchTerm,
+        'movies': movies
+    })
 
 def about(request):
     # return HttpResponse('<h1>About Movies Review Page</h1><h2>Esta es la página About de mi sitio web</h2>')
